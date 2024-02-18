c7bba0a223aab3d52253b178579e8939608f652b
Ga3im <152912210+Ga3im@users.noreply.github.com>
2024-02-18 09:33:27 +0500

    Add files via upload

--- hw2.js
+++ hw2.js
@@ -5,33 +5,33 @@ let num2 = Number(20);
 alert(num2);
 // Задание 2
 const date = new Date(2007,5,29,12,0,0);
-alert(`Год выпуска iPhone: ${date}`);
+console.log(`Год выпуска iPhone: ${date}`);
 // Задание 3
 const name = ('Brendan Eich');
-alert(`Создатель языка Java Script: ${name}`);
+console.log(`Создатель языка Java Script: ${name}`);
 // Задание 4
 let x = Number(10);
 let y = Number(2);
-alert(`Сумма ${x+y}. Разность ${x-y} Произведение ${x*y} Частное ${x/y}`);
+console.log(`Сумма ${x+y}. Разность ${x-y} Произведение ${x*y} Частное ${x/y}`);
 // Задание 5
 let result = y**5
-alert(result);
+console.log(result);
 //Задание 6
 let a = 9;
 let b = 2;
-alert(`Отстаток от деления: ${a%b}`);
+console.log(`Отстаток от деления: ${a%b}`);
 //Задание 7
 let num = 1;
-num = num + 5;
-num = num - 3;
-num = num * 7;
-num = num / 3;
-num = num + 1;
-num = num - 1;
-alert(num);
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+console.log(num);
 //Задание 8
 let age1 = prompt('Сколько вам лет?');
-alert(`You are ${age1} y. o.`);
+console.log(`You are ${age1} y. o.`);
 //Задание 9.0
 const user = {
     name: String('Gazim'), 
@@ -46,7 +46,7 @@ user.age = 24;  //Задание 9.2
 delete user.CityOfResidence;  //Задание 9.3
 
 let info = prompt('Какую информацию хотите узнать o пользователе?', "name", "age", "isAdmin");  //Задание 9.4
-alert(user[info]);
+console.log(user[info]);
 //Задание 10
 let nameuser = prompt('Как тебя зовут?');
-alert(`Привет ${nameuser}!`);
+console.log(`Привет ${nameuser}!`);
